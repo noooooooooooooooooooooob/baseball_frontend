54787d9ac8b00ff7b1d01a1d774a42689057d758
nimuseel <l08c0739@gmail.com>
2023-02-06 22:48:54 +0900

    feat: 로그인 페이지를 기본 페이지로

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/account/signin',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
