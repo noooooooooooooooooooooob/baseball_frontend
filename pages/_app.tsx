dcf70941c325deb552200df5819f42443245ac91
nimuseel <l08c0739@gmail.com>
2023-02-11 21:13:07 +0900

    refactor: 토큰 체크 후 홈으로 이동시키는 코드 _app으로 응집

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useToken } from '../hooks/useToken';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const { hasToken } = useToken();
+
+  useEffect(() => {
+    if (hasToken()) {
+      router.replace('/home');
+    }
+  }, [hasToken, router]);
+
   return <Component {...pageProps} />;
 }
--- pages/account/signin.tsx
+++ pages/account/signin.tsx
@@ -1,17 +1,5 @@
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import SigninForm from '../../components/account/SigninForm';
-import { useToken } from '../../hooks/useToken';
 
 export default function Signin() {
-  const router = useRouter();
-  const { hasToken } = useToken();
-
-  useEffect(() => {
-    if (hasToken()) {
-      router.replace('/home');
-    }
-  }, [hasToken, router]);
-
   return <SigninForm isSignin />;
 }
