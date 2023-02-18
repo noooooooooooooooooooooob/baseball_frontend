015a9276ac98a8c71e80f0fc3ee5ae950d7547ba
nimuseel <l08c0739@gmail.com>
2023-02-18 20:21:14 +0900

    feat: home ui prototype

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,11 +1,61 @@
-import { message } from 'antd';
+import { Card, message } from 'antd';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import HStack from '../components/account/common/HStack';
+import VStack from '../components/account/common/VStack';
 import { useToken } from '../hooks/useToken';
 
 export default function Home() {
   const { hasToken } = useToken();
   const router = useRouter();
+
+  const data = [
+    {
+      id: '1',
+      home: {
+        title: '두산',
+        score: 10,
+        // logo
+      },
+      away: {
+        title: 'NC',
+        score: 8,
+        // logo
+      },
+      date: '2022-01-01',
+      place: '잠실',
+    },
+    {
+      id: '2',
+      home: {
+        title: 'SK',
+        score: 1,
+        // logo
+      },
+      away: {
+        title: '키움',
+        score: 5,
+        // logo
+      },
+      date: '2022-02-01',
+      place: '잠실',
+    },
+    {
+      id: '3',
+      home: {
+        title: '롯데',
+        score: 9,
+        // logo
+      },
+      away: {
+        title: 'SSG',
+        score: 5,
+        // logo
+      },
+      date: '2022-03-01',
+      place: '잠실',
+    },
+  ];
 
   useEffect(() => {
     if (!hasToken()) {
@@ -16,7 +66,27 @@ export default function Home() {
 
   return (
     <div>
-      <span>Home Page</span>
+      {data.map((game) => (
+        <Card key={game.id}>
+          <VStack alignItems="center" justifyContent="center" spacing={8}>
+            <HStack alignItems="center" justifyContent="center" spacing={24}>
+              <VStack alignItems="center" justifyContent="center" spacing={4}>
+                <span>{game.home.title}</span>
+                <span>{game.home.score}</span>
+              </VStack>
+              <h3 style={{ margin: 0 }}>VS</h3>
+              <VStack alignItems="center" justifyContent="center" spacing={4}>
+                <span>{game.away.title}</span>
+                <span>{game.away.score}</span>
+              </VStack>
+            </HStack>
+            <VStack alignItems="center" justifyContent="center" spacing={2}>
+              <span>{game.date}</span>
+              <span>{game.place}</span>
+            </VStack>
+          </VStack>
+        </Card>
+      ))}
     </div>
   );
 }
